21fadc24bdca3ad86eae391431fad8e1e55cd2fa
Karthik R <124003154+Karthik-443@users.noreply.github.com>
2024-04-08 14:48:46 -0400

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ if [[ $GIT_BRANCH == 'origin/main' ]]; then
 docker tag project:v1 karthikramachandren/prod
 docker push karthikramachandren/prod
 # Docker login
-elif [[ $GIT_BRANCH == 'origin/dev' ]]; then
+elif [[ $GIT_BRANCH == 'origin/main' ]]; then
 docker tag project:v1 karthikramachandren/dev
 docker push karthikramachandren/dev
 fi
