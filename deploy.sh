e9157c8d1444929be113fa5391ca1e9cb04a7417
Karthik R <124003154+Karthik-443@users.noreply.github.com>
2024-04-08 14:50:09 -0400

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 docker login -u karthikramachandren -p dckr_pat_HfqjPQWNZthTxuMZsAS9mt2Yld4
 # Tag the image with the Docker Hub repository name and version
-if [[ $GIT_BRANCH == 'origin/main' ]]; then
+if [[ $GIT_BRANCH == 'origin/dev' ]]; then
 docker tag project:v1 karthikramachandren/prod
 docker push karthikramachandren/prod
 # Docker login
-elif [[ $GIT_BRANCH == 'origin/main' ]]; then
+elif [[ $GIT_BRANCH == 'origin/dev' ]]; then
 docker tag project:v1 karthikramachandren/dev
 docker push karthikramachandren/dev
 fi
